0c5065076b04d24e119928d224dc4a93963513c7
dosbenjamin <dosbenjamin@gmail.com>
2021-03-28 17:59:27 +0200

    feat(discord): add help and audio command + no result handling

--- index.js
+++ index.js
@@ -2,29 +2,40 @@ import Discord from 'discord.js'
 import dotenv from 'dotenv'
 import fetch from 'node-fetch'
 import fs from 'fs'
-import path from 'path'
 
 dotenv.config()
 
 const {
   API_URL,
-  TOKEN
+  DISCORD_TOKEN
 } = process.env
 
 const bot = new Discord.Client()
 
-bot.on('message', async ({ content, member, channel }) => {
-  const apiResponse = await fetch(`${ API_URL }/${ content }`)
-  const [{ url }] = await apiResponse.json()
 
+const messageHandler = async ({ content, member, channel }) => {
+  if (content === '!jungle help') return channel.send(`
+    ℹ Pour ajouter des sons, rendez-vous ici : https://jungle-soundbot.netlify.app`
+  )
+
+  const request = content.replace('!jungle ', '')
+  const apiResponse = await fetch(`${ API_URL }/${ request }`)
+  const json = await apiResponse.json()
+
+  if (!json.length) return channel.send('Aucun résulat 😥')
+
+  const [{ url, author, command }] = json
   const cloudinaryResponse = await fetch(url)
   const buffer = await cloudinaryResponse.buffer()
-
-  const connection = await member.voice.channel.join()
-
   fs.writeFileSync('audio.ogg', buffer)
 
+  const connection = await member.voice.channel.join()
   const dispatcher = connection.play('./audio.ogg', { volume: 1 })
-})
 
-bot.login(TOKEN)
+  channel.send(`▶ Son en cours ➜ **${ command }** ajouté par **${ author }**`)
+}
+
+bot.on('message', message => message.content
+  .startsWith('!jungle ') && messageHandler(message))
+
+bot.login(DISCORD_TOKEN)
